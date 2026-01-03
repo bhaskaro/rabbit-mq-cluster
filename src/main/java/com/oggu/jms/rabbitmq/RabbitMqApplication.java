eb8dddcb641807e02276d58488459079afa0bdf3
Vijay <bhaskaro@gmail.com>
2026-01-02 17:53:38 -0800

    Enabled scheduling

--- src/main/java/com/oggu/jms/rabbitmq/RabbitMqApplication.java
+++ src/main/java/com/oggu/jms/rabbitmq/RabbitMqApplication.java
@@ -2,8 +2,10 @@ package com.oggu.jms.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitMqApplication {
 
 	public static void main(String[] args) {
